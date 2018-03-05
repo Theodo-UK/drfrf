1a0790e08d67f8432e219211d0fc3debfbda1589
Nathan Gaberel <nathang@theodo.co.uk>
2018-03-05 19:08:57 +0000

    Test object updates.

--- lib/py/mixins.py
+++ lib/py/mixins.py
@@ -1,11 +1,26 @@
 from rest_framework import status
-from rest_framework.decorators import list_route
+from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
 
 
 class ValidateMixin(object):
-    @list_route(methods=['post'])
-    def validate(self, request):
+    validate_url_path = 'validate'
+
+    @detail_route(methods=['put', 'patch'], url_path=validate_url_path)
+    def validate_update(self, request, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=partial
+        )
+        serializer.is_valid(raise_exception=True)
+
+        return Response({}, status=status.HTTP_200_OK)
+
+    @list_route(methods=['post'], url_path=validate_url_path)
+    def validate_create(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
