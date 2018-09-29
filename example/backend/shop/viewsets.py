a5bcba677caf166ce206557777929635cddf6bec
Nathan Gaberel <nathan@gnab.fr>
2018-09-29 15:26:55 +0100

    Don't include route decorators in backend lib.

--- example/backend/shop/viewsets.py
+++ example/backend/shop/viewsets.py
@@ -1,5 +1,6 @@
 from drfrf import ValidateCreateMixin
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
 from shop.models import CartItem
 from shop.serializers import CartItemSerializer
 
@@ -7,3 +8,7 @@ from shop.serializers import CartItemSerializer
 class CartItemViewSet(ValidateCreateMixin, viewsets.ModelViewSet):
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
+
+    @list_route(methods=["post"], url_path="validate")
+    def validate(self, request):
+        return self.validate_create(request)
--- lib/py/drfrf/mixins.py
+++ lib/py/drfrf/mixins.py
@@ -1,12 +1,8 @@
 from rest_framework import status
-from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
 
 
 class ValidateCreateMixin(object):
-    validate_url_path = "validate"
-
-    @list_route(methods=["post"], url_path=validate_url_path)
     def validate_create(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -15,9 +11,6 @@ class ValidateCreateMixin(object):
 
 
 class ValidateUpdateMixin(object):
-    validate_url_path = "validate"
-
-    @detail_route(methods=["put", "patch"], url_path=validate_url_path)
     def validate_update(self, request, **kwargs):
         partial = kwargs.pop("partial", False)
         instance = self.get_object()
